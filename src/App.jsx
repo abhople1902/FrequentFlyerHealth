bfd44cf1549c51bd0583ccdda816673af3db3405
Ayush Bhople <77747115+abhople1902@users.noreply.github.com>
2023-08-06 23:54:19 +0530

    dark1

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,4 @@
 import React from 'react'
-import ReactDOM from 'react-dom/client'
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
 import {
   NavBarHeader2,
   HeroLayout2,
@@ -10,8 +6,10 @@ import {
 
 import './App.css'
 
+
+
+
 function App() {
-  const [count, setCount] = useState(0)
 
   return (
     <>
